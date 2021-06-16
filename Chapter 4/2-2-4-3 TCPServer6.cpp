5a4f31c13e4457aa5b8d7682435784d896a06db6
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-06-17 08:03:43 +0900

    Rename 2-2-4-3 TCPServer6.cpp to Chapter 4/2-2-4-3 TCPServer6.cpp

--- Chapter 4/2-2-4-3 TCPServer6.cpp
+++ Chapter 4/2-2-4-3 TCPServer6.cpp
@@ -1,4 +1,4 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS // ÃÖ½Å VC++ ÄÄÆÄÀÏ ½Ã °æ°í ¹æÁö
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // ìµœì‹  VC++ ì»´íŒŒì¼ ì‹œ ê²½ê³  ë°©ì§€
 #pragma comment(lib, "ws2_32")
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -8,7 +8,7 @@
 #define SERVERPORT 9000
 #define BUFSIZE    512
 
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â ÈÄ Á¾·á
+// ì†Œì¼“ í•¨ìˆ˜ ì˜¤ë¥˜ ì¶œë ¥ í›„ ì¢…ë£Œ
 void err_quit(char *msg)
 {
     LPVOID lpMsgBuf;
@@ -22,7 +22,7 @@ void err_quit(char *msg)
     exit(1);
 }
 
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â
+// ì†Œì¼“ í•¨ìˆ˜ ì˜¤ë¥˜ ì¶œë ¥
 void err_display(char *msg)
 {
     LPVOID lpMsgBuf;
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 {
     int retval;
 
-    // À©¼Ó ÃÊ±âÈ­
+    // ìœˆì† ì´ˆê¸°í™”
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
         return 1;
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
     retval = listen(listen_sock, SOMAXCONN);
     if (retval == SOCKET_ERROR) err_quit("listen()");
 
-    // µ¥ÀÌÅÍ Åë½Å¿¡ »ç¿ëÇÒ º¯¼ö
+    // ë°ì´í„° í†µì‹ ì— ì‚¬ìš©í•  ë³€ìˆ˜
     SOCKET client_sock;
     SOCKADDR_IN6 clientaddr;
     int addrlen;
@@ -76,16 +76,16 @@ int main(int argc, char *argv[])
             break;
         }
 
-        // Á¢¼ÓÇÑ Å¬¶óÀÌ¾ğÆ® Á¤º¸ Ãâ·Â
+        // ì ‘ì†í•œ í´ë¼ì´ì–¸íŠ¸ ì •ë³´ ì¶œë ¥
         char ipaddr[50];
         DWORD ipaddrlen = sizeof(ipaddr);
         WSAAddressToString((SOCKADDR *)&clientaddr, sizeof(clientaddr),
             NULL, ipaddr, &ipaddrlen);
-        printf("\n[TCP ¼­¹ö] Å¬¶óÀÌ¾ğÆ® Á¢¼Ó: %s\n", ipaddr);
+        printf("\n[TCP ì„œë²„] í´ë¼ì´ì–¸íŠ¸ ì ‘ì†: %s\n", ipaddr);
 
-        // Å¬¶óÀÌ¾ğÆ®¿Í µ¥ÀÌÅÍ Åë½Å
+        // í´ë¼ì´ì–¸íŠ¸ì™€ ë°ì´í„° í†µì‹ 
         while (1) {
-            // µ¥ÀÌÅÍ ¹Ş±â
+            // ë°ì´í„° ë°›ê¸°
             retval = recv(client_sock, buf, BUFSIZE, 0);
             if (retval == SOCKET_ERROR) {
                 err_display("recv()");
@@ -94,11 +94,11 @@ int main(int argc, char *argv[])
             else if (retval == 0)
                 break;
 
-            // ¹ŞÀº µ¥ÀÌÅÍ Ãâ·Â
+            // ë°›ì€ ë°ì´í„° ì¶œë ¥
             buf[retval] = '\0';
             printf("[TCP/%s] %s\n", ipaddr, buf);
 
-            // µ¥ÀÌÅÍ º¸³»±â
+            // ë°ì´í„° ë³´ë‚´ê¸°
             retval = send(client_sock, buf, retval, 0);
             if (retval == SOCKET_ERROR) {
                 err_display("send()");
@@ -108,13 +108,13 @@ int main(int argc, char *argv[])
 
         // closesocket()
         closesocket(client_sock);
-        printf("[TCP ¼­¹ö] Å¬¶óÀÌ¾ğÆ® Á¾·á: %s\n", ipaddr);
+        printf("[TCP ì„œë²„] í´ë¼ì´ì–¸íŠ¸ ì¢…ë£Œ: %s\n", ipaddr);
     }
 
     // closesocket()
     closesocket(listen_sock);
 
-    // À©¼Ó Á¾·á
+    // ìœˆì† ì¢…ë£Œ
     WSACleanup();
     return 0;
 }
